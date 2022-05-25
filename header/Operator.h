0e750fd4142e162a0ececf11c61c991d3232b6ea
Vijay Chand <VijayChand144@Gmail.com>
2022-05-25 16:23:03 -0700

    cleaned comments

--- header/Operator.h
+++ header/Operator.h
@@ -22,13 +22,11 @@ class Operator : public TwoBodyOperator {
 
         // Constructor
         Operator(){};
-        // Operator(ModelSpace modelspace1 = ModelSpace(), int rankJ1=0, int rankP1=1, int rankZ1=0);
         Operator(ModelSpace modelspace1, int rankJ1, int rankP1, int rankZ1);
 
         void set_hamiltonian(bool one_body=true, bool two_body=true);
         void set_one_body_ham();
         void set_two_body_ham();
-        // double g(double *r, size_t dim, void *params);
         double _coulomb(Orbit& oa, Orbit& ob, Orbit& oc, Orbit& od, int J);
         void orthogonalize(bool scalar=true);
         void print_operator();
