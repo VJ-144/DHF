bbe67c25fde6f1844300e51706e8ec1918051e30
Vijay Chand <VijayChand144@Gmail.com>
2022-05-31 16:21:24 -0700

    Correct type of 2bme, i.e. v

--- lib/TwoBodyOperator.h
+++ lib/TwoBodyOperator.h
@@ -14,14 +14,16 @@ class TwoBodyOperatorChannel : public TwoBodySpace {
         Mat<double> MEs;
 
         // Constructor
-        TwoBodyOperatorChannel(TwoBodyChannel chbra1=TwoBodyChannel(), TwoBodyChannel chket1=TwoBodyChannel());
+        TwoBodyOperatorChannel(){};
+        // TwoBodyOperatorChannel(TwoBodyChannel chbra1=TwoBodyChannel(), TwoBodyChannel chket1=TwoBodyChannel());
+        TwoBodyOperatorChannel(TwoBodyChannel chbra1, TwoBodyChannel chket1);
 
         int get_2bme(int idxbra, int idxket);
         int get_2bme_orbit_indices(int a, int b, int c, int d);
         int get_2bme_orbits(Orbit oa, Orbit ob, Orbit oc, Orbit od);
-        void set_2bme(int idxbra, int idxket, int v);
-        void set_2bme_orbit_indices(int a, int b, int c, int d, int v);
-        void set_2bme_orbits(Orbit oa, Orbit ob, Orbit oc, Orbit od, int v);
+        void set_2bme(int idxbra, int idxket, double v);
+        void set_2bme_orbit_indices(int a, int b, int c, int d, double v);
+        void set_2bme_orbits(Orbit oa, Orbit ob, Orbit oc, Orbit od, double v);
 };
 
 
@@ -42,10 +44,10 @@ class TwoBodyOperator : public TwoBodyOperatorChannel {
         int get_2bme_orbitsJ(Orbit oa, Orbit ob, Orbit oc, Orbit od, int Jab, int Jcd);
         int get_2bme_orbit_indicesJ(int a, int b, int c, int d, int Jab, int Jcd);
 
-        void set_2bme(int ichbra, int ichket, int idxbra, int idxket, int v);
-        void set_2bme_orbit_indices(int ichbra, int ichket, int a, int b, int c, int d, int v);
-        void set_2bme_orbits(int ichbra, int ichket, Orbit oa, Orbit ob, Orbit oc, Orbit od, int v);
-        void set_2bme_orbitsJ(Orbit oa, Orbit ob, Orbit oc, Orbit od, int Jab, int Jcd, int v);
-        void set_2bme_orbit_indicesJ(int a, int b, int c, int d, int Jab, int Jcd, int v);
+        void set_2bme(int ichbra, int ichket, int idxbra, int idxket, double v);
+        void set_2bme_orbit_indices(int ichbra, int ichket, int a, int b, int c, int d, double v);
+        void set_2bme_orbits(int ichbra, int ichket, Orbit oa, Orbit ob, Orbit oc, Orbit od, double v);
+        void set_2bme_orbitsJ(Orbit oa, Orbit ob, Orbit oc, Orbit od, int Jab, int Jcd, double v);
+        void set_2bme_orbit_indicesJ(int a, int b, int c, int d, int Jab, int Jcd, double v);
         void print_two_body_operator();
 };
